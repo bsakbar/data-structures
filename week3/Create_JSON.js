f25e1686dc3ed043633f1ecd355e8e94705af8ed
EC2 Default User <ec2-user@ip-172-31-13-56.ec2.internal>
2018-09-28 00:25:34 +0000

    assignment3

---
+++ week3/Create_JSON.js
@@ -0,0 +1,26 @@
+var fs = require('fs');
+var cheerio = require('cheerio');
+var content = fs.readFileSync('../week1/data/m02.txt');
+var $ = cheerio.load(content);
+var streetAddress = [];
+var addressline1, addressline2;
+
+// https://stackoverflow.com/questions/8935632/check-if-character-is-number
+function isNumeric(s) {
+    return !isNaN(s - parseFloat(s));
+}
+
+
+$('h4').each(function(i, elem) {
+    
+    addressline1 = $(elem).parent().text().split('\n')[3].trim()
+    addressline2 = $(elem).parent().text().split('\n')[4].trim()
+    
+    console.log(addressline1);
+    console.log(addressline2);
+    if ( isNumeric(addressline1[0]) ){
+        streetAddress.push(addressline1 + ' ' + addressline2);
+    }
+});
+
+fs.writeFileSync('streetAddress.json', JSON.stringify(streetAddress));
