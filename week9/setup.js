91f5636b235a00ab3020f12df62d1c739df384c1
EC2 Default User <ec2-user@ip-172-31-13-56.ec2.internal>
2018-11-21 03:43:16 +0000

    week9

---
+++ week9/setup.js
@@ -0,0 +1,21 @@
+const { Client } = require('pg');
+
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'batool_2018_db';
+db_credentials.host = process.env.AWSRDS_EP;
+db_credentials.database = 'batool_photon_db';
+db_credentials.password = process.env.AWSRDS_PW;
+db_credentials.port = 5432;
+
+// Connect to the AWS RDS Postgres database
+const client = new Client(db_credentials);
+client.connect();
+
+// Sample SQL statement to create a table: 
+var thisQuery = "CREATE TABLE sensorData ( sensorValue boolean, sensorTime timestamp DEFAULT current_timestamp );";
+
+client.query(thisQuery, (err, res) => {
+    console.log(err, res);
+    client.end();
+});
